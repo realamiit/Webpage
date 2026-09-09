41ccdb67662f2d8dc8bf94f4a9a1c7616e24c31e
Amit Gupta <amitgupta4263@gmail.com>
2026-09-09 23:47:59 +0530

    Learn The use of how to proint variables {} in jsx

--- 02-Components/src/App.jsx
+++ 02-Components/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+ 
+const user = 'Amit'
+const age = 23
+const location = "Greater Noida"
+const course = "MCA"
+const College = "G.L Bajaj Instuite of Tecnology & Management"
+
 
 const App = () => {
   return (
     <>
     <div className='card'>
-      <h1>Amit Gupta</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, quos!</p>
+      <h1>{user} Gupta</h1>
+      <p>I Am {age} year old!!</p>
+      <p>I live in {location} And I' am persuing My Post Graduation {course} from {College}  </p>
     </div>
     </>
   )
---
+++ 02-Components/src/comopnents/Card.jsx
@@ -0,0 +1,6 @@
+function Card(){
+    return 'hyy im am your Card'
+
+}
+
+export default Card;
