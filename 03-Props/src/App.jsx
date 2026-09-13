911a0787a7b125314e4ab5c844cfb20b78d1674d
Amit Gupta <amitgupta4263@gmail.com>
2026-09-13 23:28:51 +0530

    Addd some components to improve som visibility of the cards

--- 03-Props/src/App.jsx
+++ 03-Props/src/App.jsx
@@ -24,6 +24,7 @@ const App = () => {
       <Card user='Mehak' age={21} img='https://images.unsplash.com/photo-1556229010-6c3f2c9ca5f8?w=600' />
       <Card user='Kritika' age={20} img='https://images.unsplash.com/photo-1596462502278-27bfdc403348?w=600' />
       <Card user='Sanjana' age={24} img='https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?w=600' />
+      <Card user='Ananya' age={21} img='https://images.unsplash.com/photo-1586495777744-4413f21062fa?w=600' />
     </div>
   )
 }
