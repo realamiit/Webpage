c4de92b965fb9186d66ee343768b6bdd43797f83
Amit Gupta <amitgupta4263@gmail.com>
2026-09-13 16:39:47 +0530

    Add some card components

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -7,12 +7,6 @@ const App = () => {
     <div className='parent'>
       <Card />
       <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
 
     </div>
   )
