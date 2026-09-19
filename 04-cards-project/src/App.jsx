50381b46ecf091694193ea37ee63b94cef2b9f15
Amit Gupta <amitgupta4263@gmail.com>
2026-09-20 01:06:07 +0530

    Dlelet User.jsx because no need it was just for practice also

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -5,16 +5,216 @@ import User from './components/User'
 
 const App = () => {
 
-  const arr = [
-    20,30,40
-  ];
+  const jobs = [
+  {
+    brandLogo: "https://logo.clearbit.com/google.com",
+    companyName: "Google",
+    datePosted: "2 days ago",
+    post: "Software Engineer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$28/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/microsoft.com",
+    companyName: "Microsoft",
+    datePosted: "1 week ago",
+    post: "Frontend Developer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$32/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/amazon.com",
+    companyName: "Amazon",
+    datePosted: "3 days ago",
+    post: "Backend Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$26/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/meta.com",
+    companyName: "Meta",
+    datePosted: "5 days ago",
+    post: "React Developer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$30/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/apple.com",
+    companyName: "Apple",
+    datePosted: "1 week ago",
+    post: "iOS Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$38/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/netflix.com",
+    companyName: "Netflix",
+    datePosted: "10 days ago",
+    post: "Full Stack Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$42/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/adobe.com",
+    companyName: "Adobe",
+    datePosted: "4 days ago",
+    post: "UI Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$31/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/salesforce.com",
+    companyName: "Salesforce",
+    datePosted: "6 days ago",
+    post: "Java Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$27/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/ibm.com",
+    companyName: "IBM",
+    datePosted: "2 weeks ago",
+    post: "Cloud Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$30/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/oracle.com",
+    companyName: "Oracle",
+    datePosted: "8 days ago",
+    post: "Java Software Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$29/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/intel.com",
+    companyName: "Intel",
+    datePosted: "3 days ago",
+    post: "Software Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$25/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/nvidia.com",
+    companyName: "NVIDIA",
+    datePosted: "1 week ago",
+    post: "AI/ML Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$40/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/cisco.com",
+    companyName: "Cisco",
+    datePosted: "9 days ago",
+    post: "Network Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$28/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/uber.com",
+    companyName: "Uber",
+    datePosted: "5 days ago",
+    post: "Backend Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$35/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/spotify.com",
+    companyName: "Spotify",
+    datePosted: "12 days ago",
+    post: "Frontend Engineer",
+    tag1: "Part Time",
+    tag2: "Junior Level",
+    pay: "$24/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/airbnb.com",
+    companyName: "Airbnb",
+    datePosted: "2 weeks ago",
+    post: "Full Stack Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$33/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/tesla.com",
+    companyName: "Tesla",
+    datePosted: "6 days ago",
+    post: "Software Engineer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$27/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/accenture.com",
+    companyName: "Accenture",
+    datePosted: "4 days ago",
+    post: "MERN Stack Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$23/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/cognizant.com",
+    companyName: "Cognizant",
+    datePosted: "1 week ago",
+    post: "Software Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$25/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/jpmorganchase.com",
+    companyName: "JPMorgan Chase",
+    datePosted: "10 days ago",
+    post: "Software Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$34/hour",
+    location: "Mumbai, India"
+  }
+];
 
+console.log(jobs)
   
   return (
     <div className='parent'>
-    {arr.map(function(elem){
-    return <User name={elem}/>
-  }) }
+    {jobs.map(function(elem) {
+      return <Card />
+    })}
     </div>
   )
 }
