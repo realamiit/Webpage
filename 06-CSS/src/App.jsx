e9aba018461a409f2f0dfdfc929c31c512dbbad1
Amit Gupta <amitgupta4263@gmail.com>
2026-09-20 16:11:41 +0530

    Make another file wihich is the name is CSS to learn Css and there properties and also make in src Css other files

---
+++ 06-CSS/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+import Button from './components/Button'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Button />
+    </div>
+  
+  )
+}
+
+export default App
---
+++ 06-CSS/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Styles/Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+
+      <h1>Amit is here </h1>
+      <button className='btn'>Here me </button>
+    </div>
+  )
+}
+
+export default Header
