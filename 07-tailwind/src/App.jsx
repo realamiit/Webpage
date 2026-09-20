75cf2874c7df1e6c1208008ff0d63e7e76b7ed25
Amit Gupta <amitgupta4263@gmail.com>
2026-09-20 22:12:46 +0530

    this is the setup and little bit of use Tailwind css

--- 07-tailwind/src/App.jsx
+++ 07-tailwind/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className='h-screen w-full bg-gray-900'>
+      <h1>Hello this is amit here </h1>
     </div>
   )
 }
